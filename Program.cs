84cb6db64181a87e663a2741380467c46eeb707c
vivianmunguia <andy_munguia@hotmail.com>
2024-06-09 14:57:01 -0700

    Compound delegate was created

--- Program.cs
+++ Program.cs
@@ -1,8 +1,34 @@
-﻿MiDelegado f = delegate (int i, string s)
+﻿static void func1(int arg1, int arg2)
 {
-    return i + 100;
-};
+    string resultado = (arg1 + arg2).ToString();
+    Console.WriteLine(resultado);
+}
 
-int resultado = f(250, "Es Vaxi Drez");
+static void func2(int arg1, int arg2)
+{
+    string resultado = (arg1 * arg2).ToString();
+    Console.WriteLine(resultado);
+}
 
-public delegate int MiDelegado(int i, string s);
+MiDelegado f1 = func1;
+MiDelegado f2 = func2;
+
+MiDelegado f1f2 = f1 + f2;
+int arg1 = 10;
+int arg2 = 20;
+
+Console.WriteLine("Se está ejecutando el primer delegate");
+f1(arg1, arg2);
+
+Console.WriteLine("Se está ejecutando el segundo delegate");
+f2(arg1, arg2);
+
+Console.WriteLine("Se están ejecutando el primer y segundo delegate al mismo tiempo");
+f1f2(arg1, arg2);
+
+//f1f2 = f1f2 - f2;
+f1f2 -= f2;
+Console.WriteLine("Ejecutando la concatenacion pero sin el f2");
+f1f2(arg1, arg2);
+
+public delegate void MiDelegado(int arg1, int arg2);
--- obj/Debug/net7.0/App03.AssemblyInfo.cs
+++ obj/Debug/net7.0/App03.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App03")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f72a6229b1f2158fb834a389d376fc9433ca0c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b322e0d927f3c5a7f6c9dbad1d202f8bce1f652")]
 [assembly: System.Reflection.AssemblyProductAttribute("App03")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App03")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
